e825cba050d3d12e6aa5df38962c940a06ae364a
gefymarcos <gefymarcos@gmail.com>
2023-03-04 12:09:35 -0300

    feat: ajustando visual e animacoes

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,62 @@
 <script lang="ts">
-  import githubLogo from './assets/github.svg'
-  import code4tubaLogo from './assets/code4tuba.png'
+  import githubLogo from "./assets/github.svg";
+  import gmailLogo from "./assets/gmail.svg";
+  import twitterLogo from "./assets/twitter.svg";
+  import linkedinLogo from "./assets/linkedin.svg";
+  import code4tubaLogo from "./assets/code4tuba.svg";
+  const title = `Olá humano!`;
+  const style = (index) => {
+    return 0 + index / 13 + "s";
+  };
 </script>
 
 <main>
   <div>
-    <h1>@gefymarcos</h1>
-    <a href="https://code4tuba.com.br" target="_blank" rel="noreferrer"> 
-      <img src="{code4tubaLogo}" class="logo code4tuba" alt="Code4tuba Logo" />
+    <h1>
+      {#each title as char, i}
+        <span aria-hidden="true" style="animation-delay: {style(i + 8)};">
+          {#if char}
+            {char}
+          {/if}
+          {#if i === 3}
+            <span>&nbsp;</span>
+          {/if}
+        </span>
+      {/each}
+      <span class="cursor" />
+    </h1>
+
+    <a href="https://twitter.com/gefymarcos" target="_blank" rel="noreferrer">
+      <img src={twitterLogo} class="logo github" alt="Github Logo" />
     </a>
-    <a href="https://github.com/gefymarcos" target="_blank" rel="noreferrer"> 
+    <a href="https://github.com/gefymarcos" target="_blank" rel="noreferrer">
       <img src={githubLogo} class="logo github" alt="Github Logo" />
+    </a>
+    <a
+      href="mailto:gefymarcos@gmail.com"
+      target="_blank"
+      rel="noreferrer"
+      title="Email"
+    >
+      <img src={gmailLogo} class="logo github" alt="Github Logo" />
+    </a>
+    <a
+      href="https://linkedin.com/in/geffersonmarcos"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <img src={linkedinLogo} class="logo github" alt="Github Logo" />
+    </a>
+    <a href="https://code4tuba.com.br" target="_blank" rel="noreferrer">
+      <img src={code4tubaLogo} class="logo code4tuba" alt="Code4tuba Logo" />
     </a>
   </div>
 </main>
 
 <style>
   .logo {
-    height: 7em;
-    padding: 1.5em;
+    height: 2rem;
+    padding: 1rem;
     will-change: filter;
   }
   .logo:hover {
